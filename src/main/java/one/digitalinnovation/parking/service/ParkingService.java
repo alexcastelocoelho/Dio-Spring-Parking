c23e498ae555a1a14debe02d57148147a6cd4a3a
Alex Castelo <alexcastelo437@gmail.com>
2022-11-19 21:13:11 -0300

    changing date and time format

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,11 @@ public class ParkingService {
 
     static {
         var id  = getUUid();
+        var id1  = getUUid();
         Parking parking = new Parking(id, "DMS-1232", "Sc", "CElTA", "PRETO");
+        Parking parking1 = new Parking(id1, "DMS-2345", "PA", "honda", "prata");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll() {
@@ -28,4 +32,15 @@ public class ParkingService {
     }
 
 
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUid();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }
